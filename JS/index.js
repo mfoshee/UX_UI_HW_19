d5df324dad3bfe92c564b311d4d4ead741d4e684
mfoshee <michele@micheles-MacBook-Pro.local>
2021-09-23 17:45:11 -0400

    n

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideToggle("slow");
+      $(this).text( $(this).text() == '+ more' ? "– less" : "+ more").css({"color": "#DE007A", "background-color": "white", "margin-top": "0px", "padding": "0;", "font-family": "open-sans, sans-serif", "font-weight": "600", "font-style": "normal", "font-size": "18px", "cursor": "pointer", "text-transform": "uppercase"});
+    });
+  });
+
