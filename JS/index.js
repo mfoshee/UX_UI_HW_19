dd6b526780e615d3b255fa6c280ca445cee72032
mfoshee <michele@micheles-MacBook-Pro.local>
2021-09-29 14:15:17 -0400

    hamburger

--- JS/index.js
+++ JS/index.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
 
   function openNav() {
     document.getElementById("myNav").style.width = "100%";
-  }
+  };
   
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
-  }
+  };
