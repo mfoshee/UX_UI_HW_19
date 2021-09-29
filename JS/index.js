7bc6d49360b39f4cb31b0aae6782417e4b19a4c0
mfoshee <michele@micheles-MacBook-Pro.local>
2021-09-29 13:50:22 -0400

    092921

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,13 @@ $(document).ready(function(){
     });
   });
 
+ 
+
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
